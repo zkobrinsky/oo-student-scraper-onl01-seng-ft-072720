302cd32598cbc7231d8c48c5162d0b450c0adabb
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-17 16:07:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -38,11 +38,12 @@ class Scraper
           end
         end
       end
-
-      profile_hash[:name] = d.css(".profile-name").text
-      profile_hash[:location] = d.css(".profile-location").text
       profile_hash[:profile_quote] = d.css(".profile-quote").text
       profile_hash[:bio] = d.css(".description-holder p").text
+      profile_hash[:name] = d.css(".profile-name").text
+      profile_hash[:location] = d.css(".profile-location").text
+      
+      
       # binding.pry
       profile_hash
 
