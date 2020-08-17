1dd020a78d7ecc2f5d0fa9eb098e5c47d49258c2
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-17 16:29:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
 
   def self.create_from_collection(students_array)
     # binding.pry
-    students_array.each.tap{|o| o = Student.new(o)
+    students_array.each.tap{|o|
     binding.pry}
 
   end
