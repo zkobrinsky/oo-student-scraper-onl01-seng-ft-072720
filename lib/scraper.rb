bf0ac900a18ecdb9c7cec24d9c2f2eab89b2009c
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-17 16:07:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,12 +25,6 @@ class Scraper
     profile_hash = {}
 
     profile_data.each do |d|
-      profile_hash[:name] = d.css(".profile-name").text
-      profile_hash[:location] = d.css(".profile-location").text
-      profile_hash[:profile_quote] = d.css(".profile-quote").text
-      profile_hash[:bio] = d.css(".description-holder p").text
-      # profile_hash[:blog] = d.css(".description-holder p").text
-
         d.css(".social-icon-container a").each do |x|
           case
           when x.attr("href").include?("twitter")
@@ -44,6 +38,11 @@ class Scraper
           end
         end
       end
+
+      profile_hash[:name] = d.css(".profile-name").text
+      profile_hash[:location] = d.css(".profile-location").text
+      profile_hash[:profile_quote] = d.css(".profile-quote").text
+      profile_hash[:bio] = d.css(".description-holder p").text
       # binding.pry
       profile_hash
 
