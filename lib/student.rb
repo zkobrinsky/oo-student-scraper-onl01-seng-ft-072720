dc6974fcfeddd14af976e1794eee7a8277ac5635
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-17 16:35:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -17,7 +17,7 @@ class Student
 
 
   def self.create_from_collection(students_array)
-    students_array.each {|o| o = Student.new(o)}
+    students_array.each {|o| Student.new(o)}
   end
 
   def add_student_attributes(attributes_hash)
